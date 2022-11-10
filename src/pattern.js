eed1c5be70cd0e9f7f023ee4d70aee17ca79e184
AlphaTr <alphatr@alphatr.com>
2022-11-10 09:20:43 +0800

    feat(src): remove Node Canvas dependencies

--- src/pattern.js
+++ src/pattern.js
@@ -1,4 +1,3 @@
-import { createCanvas } from 'canvas' // this is a simple shim in browsers
 import getScalingRatio from './utils/getScalingRatio'
 const isBrowser = (typeof window !== 'undefined' && typeof document !== 'undefined')
 const doc = isBrowser && document
@@ -95,7 +94,7 @@ export default class Pattern {
     const canvasOpts = { ...defaultCanvasOptions, ..._canvasOpts }
     const { points, polys, opts } = this
 
-    const canvas = destCanvas || createCanvas(opts.width, opts.height) // doc.createElement('canvas')
+    const canvas = destCanvas || doc.createElement('canvas')
     const ctx = canvas.getContext('2d')
 
     if (canvasOpts.scaling) {
